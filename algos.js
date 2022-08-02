6ceade48961318ad346a58661a23696d689cef8f
Jae Gifford <jae.giff@outlook.com>
2022-08-02 15:02:50 +0800

    added open or senior simple algo

--- algos.js
+++ algos.js
@@ -1,4 +1,24 @@
 // fizzbuzz, find the sums of products of 3 and 5
-function fizzBuzzJae() {
+function fizzBuzzJae(){
     // pass for now
 }
+
+// given a list of people's ages and handicap, determine whether they fit in the senior or open golfing category.
+// Senior == age >= 55 AND handicap > 7
+function openOrSeniorJae(data){
+    let strArray = [];
+    data.forEach(element => {
+        if ((element[0] >= 55) && (element[1] > 7)){
+            strArray.push('Senior');
+        } else {
+            strArray.push('Open');
+        };
+    });
+    return strArray;
+};
+
+// Best practices use map method.
+function openOrSeniorClever(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+  }
+  
