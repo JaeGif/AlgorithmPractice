9a5e3474f97f0934b7d8355a6382011ee2c74510
Jae Gifford <jae.giff@outlook.com>
2022-08-23 14:24:13 +0800

    mergeTwoSortedLists added

--- algos.js
+++ algos.js
@@ -267,4 +267,25 @@ const firstUniqChar = function (s) {
   return -1;
 };
 
-console.log(firstUniqChar('aabcb'));
+function mergeTwoLists(l1, l2) {
+  let merge = new ListNode();
+  let head = merge;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      merge.next = l1;
+      l1 = l1.next;
+    } else {
+      merge.next = l2;
+      l2 = l2.next;
+    }
+    merge = merge.next;
+  }
+
+  if (!l1) merge.next = l2;
+  else if (!l2) merge.next = l1;
+
+  return head.next;
+}
+
+console.log(mergeTwoLists([1, 2, 3, 4, 5], [2, 3, 4, 7, 9, 10]));
