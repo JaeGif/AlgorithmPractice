bf17ce100c787b8eccedb901c7a88cc9b06fc985
Jae Gifford <jae.giff@outlook.com>
2023-09-14 08:47:27 -0700

    67 Add Binanry

--- algos.js
+++ algos.js
@@ -1147,3 +1147,33 @@ const searchInsert = function (nums, target) {
   }
   return lo;
 };
+
+/**
+ * 67. Add Binary
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  let carry = 0;
+  let maxLength = a.length;
+
+  if (a.length > b.length) {
+    b = '0'.repeat(a.length - b.length) + b;
+  } else {
+    maxLength = b.length;
+    a = '0'.repeat(b.length - a.length) + a;
+  }
+
+  var result = '';
+
+  for (let i = maxLength - 1; i >= 0; i--) {
+    sum = parseInt(a[i]) + parseInt(b[i]) + carry;
+    result = (sum % 2) + result;
+    carry = sum >= 2 ? 1 : 0;
+  }
+  if (carry) {
+    result = '1' + result;
+  }
+  return result;
+};
