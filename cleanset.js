1f797cdd061250ec9235094baf1dc0dddfe2541c
Jacob Gifford <105343665+JaeGif@users.noreply.github.com>
2024-04-08 15:34:52 -0700

    adding LC 2 Add Two Numbers

--- cleanset.js
+++ cleanset.js
@@ -35,3 +35,36 @@ const removeDuplicates = function (nums) {
   }
   return nums.length;
 };
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+  // tail contains the first digit etc.
+  // reverse linked list, then traverse each node
+  // OR traverse linked list in order, and prepend each node
+  // in order traversal requies only 1 visit to each node,
+  // While reversal and in order traversal requires 2. 
+  let strl1 = ''
+  let strl2 = ''
+  const inOrderTraversal = (root, strList) => {
+    // prepend the value to the string
+    let current = root
+    let next;
+    while (current.next !== null) {
+    strList = current.val + strList
+    current = current.next
+    }
+    return strList
+  }
+  return Number(inOrderTraversal(l1, strl1)) + Number(inOrderTraversal(l2, strl2))
+
+};
