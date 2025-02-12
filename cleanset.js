5f27953bc15a17781fb43fc4a1cc7cf922f632f5
Jae <giffordjacob0@gmail.com>
2025-02-11 18:17:55 -0800

    cleanset

--- cleanset.js
+++ cleanset.js
@@ -1100,7 +1100,11 @@ const countBadPairs = function (nums) {
 const removeOccurrences = function (s, part) {
   // find remove substring occurences left to right until all occurrences are gone
   // return s after they are all removed
-
+  const checkMatch = (stack, part) => {
+    // checkout the last part of the stack
+    // rejoin part.length pieces of the stack and compare
+    return stack.slice(stack.length - part.length).join('') === part;
+  };
   // when things candy crush together, stack
   const stack = [];
   for (let i = 0; i < s.length; i++) {
@@ -1116,14 +1120,32 @@ const removeOccurrences = function (s, part) {
       }
     }
   }
-
   // rejoin stack
   return stack.join('');
 };
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const rob = function (nums) {
+  // do not rob adjacent houses
+  // return max money to be robbed in one night
 
-const checkMatch = (stack, part) => {
-  // checkout the last part of the stack
-  // rejoin part.length pieces of the stack and compare
-  const lastCouple = stack.slice(stack.length - part.length);
-  return stack.slice(stack.length - part.length).join('') === part;
+  if (!nums) return 0;
+  if (nums.length === 1) return nums[0];
+
+  const dp = Array(nums.length).fill(0);
+
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  // 2 7 11 11 12
+  //         ^
+  // 2 7 9 3 1
+  //         ^
+  //
+  for (let i = 2; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+  }
+
+  return dp[nums.length - 1];
 };
